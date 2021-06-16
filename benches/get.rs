99a35fec052d712b4c09eb2eaa7ae6e451c2123a
SKTT1Ryze <linuxgnulover@gmail.com>
2021-06-16 20:06:25 +0800

    add get benchs

---
+++ benches/get.rs
@@ -0,0 +1,30 @@
+//! Get 请求测试
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::async_executor::FuturesExecutor;
+use s3_bench_rs::{GetTaskBuilder, StdError, Task, TaskBuiler};
+use reqwest::Url;
+
+#[tokio::main]
+async fn get() -> Result<String, Box<StdError>> {
+    let get_task_builder = GetTaskBuilder::new(
+        "http://172.25.38.164:9000".parse::<Url>().unwrap(),
+        "ccc",
+        "WXZFwxzf123",
+        "minio"
+    );
+    let task = get_task_builder.spawn("bucket0", "test.md");
+    let text = task.run().await?;
+    Ok(text)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("Async GetObject", move |b| {
+        b.to_async(FuturesExecutor).iter(|| async {
+            let _ret = get();
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
